00f9c7f48fe61a274b363c09d4fd90fee5dc9b8d
Kaaleb <105943434+KaalebCG@users.noreply.github.com>
2023-07-06 18:45:24 +0000

    adiciona scripts  e

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 um clone de um projeto real
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-  return <h1>Cristiane, te amo muito e se voce me ama da uma risadinha!</h1>
+function Home() {
+  return <h1>Cristiane, te amo muito e se voce me ama da uma risadinha!</h1>;
+}
+
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
